f00709ea46adb29db850d62b4123df41e4b20868
Michael Nikitochkin <miry.sof@gmail.com>
2014-04-02 23:25:28 +0300

    Init commit.

---
+++ attributes/defaults.rb
@@ -0,0 +1,9 @@
+override["postgresql"]["users"] = [
+    {
+        "username"  => "teamcity",
+        "password"  => "teamcity",
+        "superuser" => "false",
+        "createdb"  => "true",
+        "login"     => "true"
+    }
+]
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'teamcity-server'
+maintainer       'Michael Nikitochkin'
+maintainer_email 'michael@jetthoughts.com'
+license          'Apache 2.0'
+description      'Installs/Configures Teamcity server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'ubuntu', '= 12.04'
